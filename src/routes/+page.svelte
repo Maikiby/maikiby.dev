015a6b8f40f244ba99ecb15a1a99715a5c70cc47
Sergio Martinez <maikiby123@gmail.com>
2024-02-19 16:56:56 -0700

    Placeholder for homepage

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,6 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+    const src = "https://3.bp.blogspot.com/-nRHdGD9U73s/U1T3myXSVQI/AAAAAAAAfTg/zFcQ41c_k_Y/s800/figure_depressed.png";
+</script>
+<h1>Welcome to my portfolio!</h1>
+<p>It is currently empty, just like my hopes of getting a job in this industry.</p>
+<img src={src} alt="depressed man sitting in fetal position" height="100px"/>
