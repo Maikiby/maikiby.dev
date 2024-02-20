9f6458b6f6e19a5d870439c88faea86ad763c5c7
Sergio Martinez <maikiby123@gmail.com>
2024-02-19 17:18:03 -0700

    formatting

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,8 @@
 <script>
-    const src = "https://3.bp.blogspot.com/-nRHdGD9U73s/U1T3myXSVQI/AAAAAAAAfTg/zFcQ41c_k_Y/s800/figure_depressed.png";
+	const src =
+		'https://3.bp.blogspot.com/-nRHdGD9U73s/U1T3myXSVQI/AAAAAAAAfTg/zFcQ41c_k_Y/s800/figure_depressed.png';
 </script>
+
 <h1>Welcome to my portfolio!</h1>
 <p>It is currently empty, just like my hopes of getting a job in this industry.</p>
-<img src={src} alt="depressed man sitting in fetal position" height="100px"/>
+<img {src} alt="depressed man sitting in fetal position" height="100px" />
