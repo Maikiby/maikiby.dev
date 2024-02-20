0dd234c25e728a5e4767a145a90e8aff7b2b8e34
Sergio Martinez <maikiby123@gmail.com>
2024-02-19 17:33:58 -0700

    added tailwindCSS

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,6 +3,12 @@
 		'https://3.bp.blogspot.com/-nRHdGD9U73s/U1T3myXSVQI/AAAAAAAAfTg/zFcQ41c_k_Y/s800/figure_depressed.png';
 </script>
 
-<h1>Welcome to my portfolio!</h1>
+<h1 class="text-header">Welcome to my portfolio!</h1>
 <p>It is currently empty, just like my hopes of getting a job in this industry.</p>
-<img {src} alt="depressed man sitting in fetal position" height="100px" />
+<img {src} alt="depressed man sitting in fetal position" height="100px" class="h-20"/>
+
+<style lang="postcss">
+	:global(html) {
+		background-color: theme(colors.gray.900);
+	}
+</style>
