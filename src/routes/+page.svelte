2d8c34c9180d5ffcfd3260790f1af210660eb5fb
Sergio Martinez <maikiby123@gmail.com>
2024-03-27 00:00:00 -0600

    test avatar component

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,10 +1,12 @@
-<script>
+<script lang="ts">
+	import { Avatar } from '@skeletonlabs/skeleton';
+
 	const src =
 		'https://3.bp.blogspot.com/-nRHdGD9U73s/U1T3myXSVQI/AAAAAAAAfTg/zFcQ41c_k_Y/s800/figure_depressed.png';
 </script>
 
 <div class="container mx-auto p-8 space-y-8">
+	<Avatar src="/profile.jpg" />
 	<p>It is currently empty, just like my hopes of getting a job in this industry.</p>
 	<img {src} alt="depressed man sitting in fetal position" height="100px" class="h-20" />
-	<img src="/profile.jpg" alt="Web Author's Profile Picture" aria-hidden="true" />
 </div>
