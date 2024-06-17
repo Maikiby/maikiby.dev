e1767eeb85bbf09f3b487a07e07d51396108ee02
Maikiby <maikiby@pm.me>
2024-06-16 20:36:58 -0600

    updated content

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,12 +1,14 @@
 <script lang="ts">
 	import { Avatar } from '@skeletonlabs/skeleton';
-
-	const src =
-		'https://3.bp.blogspot.com/-nRHdGD9U73s/U1T3myXSVQI/AAAAAAAAfTg/zFcQ41c_k_Y/s800/figure_depressed.png';
 </script>
 
 <div class="container mx-auto p-8 space-y-8">
-	<Avatar src="/profile.jpg" />
-	<p>It is currently empty, just like my hopes of getting a job in this industry.</p>
-	<img {src} alt="depressed man sitting in fetal position" height="100px" class="h-20" />
+	<h1 class="h1 font-bold inline">
+		<Avatar src="/profile.jpg" />
+		Hey there!
+	</h1>
+
+	<p>
+		This is my little portfolio website where I intend to showcase all the cool things I can do.
+	</p>
 </div>
